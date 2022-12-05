3e3e93c16e3c41b50be0540063dd64a7e89c577f
mik <mik@Firefly>
2022-12-05 15:38:00 +0100

    prepare all other days / small fixes for day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // Part 1
     let mut my_score: i32 = 0;
-    let mut opponent_score: i32 = 0;
+    let mut _opponent_score: i32 = 0;
         if let Ok(input) = std::fs::read_to_string("./input") {
         let game = input.split("\n");
         for game_round in game {
@@ -14,15 +14,15 @@ fn main() {
             match opponent_hand {
                 "A" => {
                     opponent_hand = "Rock";
-                    opponent_score += 1;
+                    _opponent_score += 1;
                 },
                 "B" => {
                     opponent_hand = "Paper";
-                    opponent_score += 2;
+                    _opponent_score += 2;
                 },
                 "C" => {
                     opponent_hand = "Scissor";
-                    opponent_score += 3;
+                    _opponent_score += 3;
                 },
                 _ => {},
             };
@@ -33,7 +33,7 @@ fn main() {
                     if opponent_hand == "Scissor" {
                         my_score += 6;
                     } else if opponent_hand == "Paper" {
-                        opponent_score += 6;
+                        _opponent_score += 6;
                     }
                 },
                 "Y" => {
@@ -42,7 +42,7 @@ fn main() {
                     if opponent_hand == "Rock" {
                         my_score += 6;
                     } else if opponent_hand == "Scissor" {
-                        opponent_score += 6;
+                        _opponent_score += 6;
                     }
                 },
                 "Z" => {
@@ -51,14 +51,14 @@ fn main() {
                     if opponent_hand == "Paper" {
                         my_score += 6;
                     } else if opponent_hand == "Rock" {
-                        opponent_score += 6;
+                        _opponent_score += 6;
                     }
                 },
                 _ => {},
             };
             if my_hand == opponent_hand {
                 my_score += 3;
-                opponent_score += 3;
+                _opponent_score += 3;
             }
         }
         println!("Part1: {}", my_score);
@@ -73,37 +73,37 @@ fn main() {
                 break;
             }
             let hands: Vec<&str> = game_round.split(" ").collect();
-            let mut my_hand = hands[1];
+            let my_hand = hands[1];
             let mut opponent_hand = hands[0];
             match opponent_hand {
                 "A" => {
                     opponent_hand = "Rock";
-                    opponent_score += 1;
+                    _opponent_score += 1;
                 },
                 "B" => {
                     opponent_hand = "Paper";
-                    opponent_score += 2;
+                    _opponent_score += 2;
                 },
                 "C" => {
                     opponent_hand = "Scissor";
-                    opponent_score += 3;
+                    _opponent_score += 3;
                 },
                 _ => {},
             };
             match my_hand {
                 "X" => { // lose
-                    opponent_score += 6;
+                    _opponent_score += 6;
                     match opponent_hand {
                         "Rock" => {
-                           my_hand = "Scissor";
+                           //my_hand = "Scissor";
                            my_score += 3; 
                         },
                         "Paper" => {
-                            my_hand = "Rock";
+                            //my_hand = "Rock";
                             my_score += 1;
                         },
                         "Scissor" => {
-                            my_hand = "Paper";
+                            //my_hand = "Paper";
                             my_score += 2;
                         },
                     _ => {},
@@ -111,18 +111,18 @@ fn main() {
                 },
                 "Y" => { // draw
                     my_score += 3;
-                    opponent_score += 3;
+                    _opponent_score += 3;
                     match opponent_hand {
                         "Rock" => {
-                            my_hand = "Rock";
+                            //my_hand = "Rock";
                             my_score += 1
                         },
                         "Paper" => {
-                            my_hand = "Paper";
+                            //my_hand = "Paper";
                             my_score += 2;
                         },
                         "Scissor" => {
-                            my_hand = "Scissor";
+                            //my_hand = "Scissor";
                             my_score += 3;
                         },
                         _ => {},
@@ -132,15 +132,15 @@ fn main() {
                     my_score += 6;
                     match opponent_hand {
                         "Rock" => {
-                            my_hand = "Paper";
+                            //my_hand = "Paper";
                             my_score += 2;
                         },
                         "Paper" => {
-                            my_hand = "Scissor";
+                            //my_hand = "Scissor";
                             my_score += 3;
                         },
                         "Scissor" => {
-                            my_hand = "Rock";
+                            //my_hand = "Rock";
                             my_score += 1;
                         },
                         _ => {},
---
+++ day9/src/main.rs
@@ -0,0 +1,21 @@
+fn part1() {
+    let mut result = "";
+    if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    //if let Ok(input) = std::fs::read_to_string("./input") {
+    // Code here
+    }
+    println!("Part1: {}", result);
+}
+fn part2() {
+    let mut result = "";
+    if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    //if let Ok(input) = std::fs::read_to_string("./input") {
+    // Code here
+    }
+    println!("Part2: {}", result);
+}
+
+fn main() {
+    part1();
+    part2();
+}

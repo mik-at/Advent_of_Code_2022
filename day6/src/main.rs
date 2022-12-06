a49fa542cc22f26bc569968f5f5070d790dfe5f4
mik <mik@Firefly>
2022-12-06 17:52:53 +0100

    day6 solved

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,16 +1,42 @@
+use itertools::Itertools;
+
 fn part1() {
-    let mut result = "";
-    if let Ok(input) = std::fs::read_to_string("./input_sample") {
-    //if let Ok(input) = std::fs::read_to_string("./input") {
-    // Code here
+    let mut buffer: Vec<char> = Vec::new();
+    let mut result = 0;
+    //if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    if let Ok(input) = std::fs::read_to_string("./input") {
+        for char in input.chars() {
+            if buffer.len() < 4 {
+                buffer.push(char);
+            } else {
+                let marker: [char; 4] = [buffer[buffer.len()-4], buffer[buffer.len()-3], buffer[buffer.len()-2], buffer[buffer.len()-1]];
+                if marker.iter().unique().count() == 4 {
+                    result = buffer.len();
+                    break;
+                }
+                buffer.push(char);
+            }
+        }
     }
     println!("Part1: {}", result);
 }
 fn part2() {
-    let mut result = "";
-    if let Ok(input) = std::fs::read_to_string("./input_sample") {
-    //if let Ok(input) = std::fs::read_to_string("./input") {
-    // Code here
+    let mut buffer: Vec<char> = Vec::new();
+    let mut result = 0;
+    //if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    if let Ok(input) = std::fs::read_to_string("./input") {
+        for char in input.chars() {
+            if buffer.len() < 14 {
+                buffer.push(char);
+            } else {
+                let marker: [char; 14] = [buffer[buffer.len()-14], buffer[buffer.len()-13], buffer[buffer.len()-12], buffer[buffer.len()-11], buffer[buffer.len()-10], buffer[buffer.len()-9], buffer[buffer.len()-8], buffer[buffer.len()-7], buffer[buffer.len()-6], buffer[buffer.len()-5], buffer[buffer.len()-4], buffer[buffer.len()-3], buffer[buffer.len()-2], buffer[buffer.len()-1]];
+                if marker.iter().unique().count() == 14 {
+                    result = buffer.len();
+                    break;
+                }
+                buffer.push(char);
+            }
+        }
     }
     println!("Part2: {}", result);
 }

6f57c194d3e81db2c8c6beae2e75c077151aecf3
mik <mik@Firefly>
2022-12-09 11:55:09 +0100

    day9 part2 not working

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,5 +1,34 @@
 use grid::*;
 
+fn move_tail(pos_h: (usize, usize), pos_t: (usize, usize)) -> (usize, usize) {
+    let mut pos_t = pos_t;
+    if pos_h.0 > pos_t.0 + 1 {
+        pos_t.0 += 1;
+        if pos_h.1 != pos_t.1 {
+            pos_t.1 = pos_h.1;
+        }
+    }
+    if pos_h.1 > pos_t.1 + 1 {
+        pos_t.1 += 1;
+        if pos_h.0 != pos_t.0 {
+            pos_t.0 = pos_h.0;
+        }
+    }
+    if pos_t.0 > 0 && pos_h.0 < pos_t.0 - 1 {
+        pos_t.0 -= 1;
+        if pos_h.1 != pos_t.1 {
+            pos_t.1 = pos_h.1;
+        }
+    }
+    if pos_t.1 > 0 && pos_h.1 < pos_t.1 - 1 {
+        pos_t.1 -= 1;
+        if pos_h.0 != pos_t.0 {
+            pos_t.0 = pos_h.0;
+        }
+    }
+    return pos_t;
+}
+
 fn part1() {
     let mut result = 0;
     //if let Ok(input) = std::fs::read_to_string("./input_sample") {
@@ -14,7 +43,6 @@ fn part1() {
                 if movements == count {
                     break;
                 }
-                grid[pos_t.0][pos_t.1] = "#";
                 match direction {
                     "R" => {
                         pos_h.1 += 1;
@@ -46,32 +74,9 @@ fn part1() {
                     }
                     _ => (),
                 };
-                if pos_h.0 > pos_t.0 + 1 {
-                    pos_t.0 += 1;
-                    if pos_h.1 != pos_t.1 {
-                        pos_t.1 = pos_h.1;
-                    }
-                }
-                if pos_h.1 > pos_t.1 + 1 {
-                    pos_t.1 += 1;
-                    if pos_h.0 != pos_t.0 {
-                        pos_t.0 = pos_h.0;
-                    }
-                }
-                if pos_t.0 > 0 && pos_h.0 < pos_t.0 - 1 {
-                    pos_t.0 -= 1;
-                    if pos_h.1 != pos_t.1 {
-                        pos_t.1 = pos_h.1;
-                    }
-                }
-                if pos_t.1 > 0 && pos_h.1 < pos_t.1 - 1 {
-                    pos_t.1 -= 1;
-                    if pos_h.0 != pos_t.0 {
-                        pos_t.0 = pos_h.0;
-                    }
-                }
+                pos_t = move_tail(pos_h, pos_t);
+                grid[pos_t.0][pos_t.1] = "#";
             }
-            grid[pos_t.0][pos_t.1] = "#";
         }
         /*dbg!(grid);
         println!("    [ \".\", \".\", \"#\", \"#\", \".\", \".\"]");
@@ -89,11 +94,96 @@ fn part1() {
     }
     println!("Part1: {}", result);
 }
+
 fn part2() {
     let mut result = 0;
-    if let Ok(input) = std::fs::read_to_string("./input_sample") {
-        //if let Ok(input) = std::fs::read_to_string("./input") {
-        // Code here
+    //if let Ok(input) = std::fs::read_to_string("./input_sample2") {
+    if let Ok(input) = std::fs::read_to_string("./input") {
+        let mut grid = grid!(["."]);
+        let mut pos_h = (0, 0);
+        let mut pos_t1 = (0, 0);
+        let mut pos_t2 = (0, 0);
+        let mut pos_t3 = (0, 0);
+        let mut pos_t4 = (0, 0);
+        let mut pos_t5 = (0, 0);
+        let mut pos_t6 = (0, 0);
+        let mut pos_t7 = (0, 0);
+        let mut pos_t8 = (0, 0);
+        let mut pos_t9 = (0, 0);
+        for h_move in input.lines() {
+            let (direction, count) = h_move.split_once(" ").unwrap();
+            let count: i32 = count.parse().unwrap();
+            for movements in 0..=count {
+                if movements == count {
+                    break;
+                }
+                match direction {
+                    "R" => {
+                        pos_h.1 += 1;
+                        if pos_h.1 == grid.cols() {
+                            grid.push_col(vec!["."; grid.rows()]);
+                        }
+                    }
+                    "L" => {
+                        if pos_h.1 == 0 {
+                            grid.insert_col(0, vec!["."; grid.rows()]);
+                            pos_t1.1 += 1;
+                            pos_t2.1 += 1;
+                            pos_t3.1 += 1;
+                            pos_t4.1 += 1;
+                            pos_t5.1 += 1;
+                            pos_t6.1 += 1;
+                            pos_t7.1 += 1;
+                            pos_t8.1 += 1;
+                            pos_t9.1 += 1;
+                        } else {
+                            pos_h.1 -= 1;
+                        }
+                    }
+                    "U" => {
+                        if pos_h.0 == 0 {
+                            grid.insert_row(0, vec!["."; grid.cols()]);
+                            pos_t1.0 += 1;
+                            pos_t2.0 += 1;
+                            pos_t3.0 += 1;
+                            pos_t4.0 += 1;
+                            pos_t5.0 += 1;
+                            pos_t6.0 += 1;
+                            pos_t7.0 += 1;
+                            pos_t8.0 += 1;
+                            pos_t9.0 += 1;
+                        } else {
+                            pos_h.0 -= 1;
+                        }
+                    }
+                    "D" => {
+                        pos_h.0 += 1;
+                        if pos_h.0 == grid.rows() {
+                            grid.push_row(vec!["."; grid.cols()]);
+                        }
+                    }
+                    _ => (),
+                };
+                pos_t1 = move_tail(pos_h, pos_t1);
+                pos_t2 = move_tail(pos_t1, pos_t2);
+                pos_t3 = move_tail(pos_t2, pos_t3);
+                pos_t4 = move_tail(pos_t3, pos_t4);
+                pos_t5 = move_tail(pos_t4, pos_t5);
+                pos_t6 = move_tail(pos_t5, pos_t6);
+                pos_t7 = move_tail(pos_t6, pos_t7);
+                pos_t8 = move_tail(pos_t7, pos_t8);
+                pos_t9 = move_tail(pos_t8, pos_t9);
+                grid[pos_t9.0][pos_t9.1] = "#";
+            }
+        }
+        dbg!(&grid);
+        for row in 0..grid.rows() {
+            for col in 0..grid.cols() {
+                if grid[row][col] == "#" {
+                    result += 1;
+                }
+            }
+        }
     }
     println!("Part2: {}", result);
 }

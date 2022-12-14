c07a4f35b5e3abb963e3a64b41d10bd453034b88
mik <mik@BlackPearl.local>
2022-12-14 16:29:37 +0100

    day14 solved

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,5 @@
+use serde_json::{value, Result, Value};
+
 fn rem_first_and_last(value: &str) -> &str {
     let mut chars = value.chars();
     chars.next();
@@ -14,57 +16,107 @@ fn rem_last(value: &str) -> &str {
     chars.next_back();
     chars.as_str()
 }
-fn extract_data(packet: &str) -> Vec<Vec<i32>> {
-    //let packet = rem_first(packet);
-    let mut data: Vec<Vec<i32>> = Vec::new();
-    let mut value = "".to_owned();
-    let mut lists: Vec<String> = Vec::new();
-    for char in packet.chars() {
-        match char as u8 {
-            b'[' => {
-                value.push(char);
-            }
-            b'0'..=b'9' => {
-                value.push(char);
-            }
-            b']' => {
-                //value.push(char);
-                lists.push(value);
-                value = "".to_owned();
-            }
-            b',' => {
-                value.push(']');
-                value.push(char);
-                value.push('[');
-            }
-            _ => (),
-        }
-    }
-    dbg!(&lists);
-    for list in lists {
-        let list = list.trim_matches(|c| c == '[' || c == ']' || c == ',');
-        let mut list_vec: Vec<i32> = Vec::new();
-        for value in list.split(",") {
-            //dbg!(&value);
-            let value = value.trim_matches(|c| c == '[' || c == ']');
-            if value != "" {
-                list_vec.push(value.parse().unwrap());
-            }
-        }
-        data.push(list_vec);
-    }
-    return data;
+fn extract_data(packet: &str) -> (Value, Value) {
+    let (left, right) = packet.split_once("\n").unwrap();
+    let left = r#"{ "data": "#.to_owned() + left + r"}";
+    let right = r#"{ "data": "#.to_owned() + right + r"}";
+    let left_data: Value = serde_json::from_str(left.as_str()).unwrap();
+    let right_data: Value = serde_json::from_str(right.as_str()).unwrap();
+    return (left_data, right_data);
 }
 
 fn part1() {
-    let mut result = "";
+    let mut result = 0;
     if let Ok(input) = std::fs::read_to_string("./input_sample") {
-        //if let Ok(input) = std::fs::read_to_string("./input") {
-        for substring in input.split("\n\n") {
-            let (left, right) = substring.split_once("\n").unwrap();
-            let left_data = extract_data(left);
-            let right_data = extract_data(right);
-            //dbg!(left_data, right_data);
+    //if let Ok(input) = std::fs::read_to_string("./input") {
+        let mut packet_index = 1;
+        for packet in input.split("\n\n") {
+            let (left, right) = extract_data(packet);
+            let mut count = 0;
+            let mut order_ok: Option<bool> = None;
+            loop {
+                let left_value = &left["data"][count];
+                let right_value = &right["data"][count];
+                let left_vec: Vec<Value> = if left_value.is_array() {
+                    left_value.as_array().unwrap().to_owned()
+                } else if left_value.is_number() {
+                    vec![left_value.to_owned()]
+                } else {
+                    Vec::new()
+                };
+                let right_vec: Vec<Value> = if right_value.is_array() {
+                    right_value.as_array().unwrap().to_owned()
+                } else if right_value.is_number() {
+                    vec![right_value.to_owned()]
+                } else {
+                    Vec::new()
+                };
+                let mut vec_iter = 0;
+                for _value in &left_vec {
+                    if packet_index == 8 {
+                        dbg!(left_vec.get(vec_iter), right_vec.get(vec_iter));
+                    }
+                    if right_vec.get(vec_iter).is_none() {
+                        println!("right ran out first");
+                        order_ok = Some(false);
+                        break;
+                    }
+
+                    let mut left_check = left_vec.get(vec_iter).unwrap();
+                    let mut right_check = right_vec.get(vec_iter).unwrap();
+
+                    while !left_check.is_number() {
+                        if left_check.is_array() {
+                            left_check = left_check.as_array().unwrap().iter().next().unwrap();
+                        }
+                    }
+                    while !right_check.is_number() {
+                        if right_check.is_array() {
+                            right_check = right_check.as_array().unwrap().iter().next().unwrap()
+                        }
+                    }
+                    let left_int = left_check.as_i64().unwrap();
+                    let right_int = right_check.as_i64().unwrap();
+                    dbg!(left_int, right_int);
+                    if left_int < right_int {
+                        println!("left is smaller");
+                        order_ok = Some(true);
+                        break;
+                    } else if left_int > right_int {
+                        println!("right is smaller");
+                        order_ok = Some(false);
+                        break;
+                    }
+                    vec_iter += 1;
+                }
+                if order_ok == None {
+                    let mut vec_iter = 0;
+                    for _value in &right_vec {
+                        if left_vec.get(vec_iter).is_none() {
+                            println!("left ran out first");
+                            order_ok = Some(true);
+                            break;
+                        }
+                        vec_iter += 1;
+                    }
+                }
+
+                if order_ok != None {
+                    println!("order_ok is set");
+                    break;
+                }
+                if right_value.is_null() && left_value.is_null() {
+                    println!("everything is null");
+                    break;
+                }
+
+                count += 1;
+            }
+            dbg!(packet_index, order_ok);
+            if order_ok == Some(true) {
+                result += packet_index;
+            }
+            packet_index += 1;
         }
     }
     println!("Part1: {}", result);
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,16 +1,264 @@
+use grid::*;
+
+fn make_cave(input: String) -> (Grid<char>, usize, usize) {
+    let mut grid: Grid<char> = Grid::from_vec(vec!['.'], 1);
+    let mut y_modifier: usize = usize::MAX;
+    for walls in input.lines() {
+        for wall_coordinates in walls.split(" -> ") {
+            let (y, _) = wall_coordinates.split_once(",").unwrap();
+            let y: usize = y.parse().unwrap();
+            if y_modifier > y {
+                y_modifier = y;
+            }
+        }
+    }
+    for walls in input.lines() {
+        for wall_coordinates in walls.split(" -> ") {
+            let (y, x) = wall_coordinates.split_once(",").unwrap();
+            let x: usize = x.parse().unwrap();
+            let mut y: usize = y.parse().unwrap();
+            y -= y_modifier;
+            while x + 1 > grid.rows() {
+                grid.push_row(vec!['.'; grid.cols()]);
+            }
+            while y + 1 > grid.cols() {
+                grid.push_col(vec!['.'; grid.rows()]);
+            }
+        }
+    }
+    grid.push_row(vec!['.'; grid.cols()]);
+    grid.push_col(vec!['.'; grid.rows()]);
+    for walls in input.lines() {
+        let mut last = false;
+        let mut last_x: usize = 0;
+        let mut last_y: usize = 0;
+        for wall_coordinates in walls.split(" -> ") {
+            let (y, x) = wall_coordinates.split_once(",").unwrap();
+            let x: usize = x.parse().unwrap();
+            let mut y: usize = y.parse().unwrap();
+            y -= y_modifier;
+            grid[x][y] = '#';
+            if last == false {
+                last = true;
+                last_x = x;
+                last_y = y;
+            } else {
+                if x != last_x {
+                    if x > last_x {
+                        for u in last_x..x {
+                            grid[u][y] = '#';
+                        }
+                    } else {
+                        for u in x..last_x {
+                            grid[u][y] = '#';
+                        }
+                    }
+                } else {
+                    if y > last_y {
+                        for u in last_y..y {
+                            grid[x][u] = '#';
+                        }
+                    } else {
+                        for u in y..last_y {
+                            grid[x][u] = '#';
+                        }
+                    }
+                }
+                last_x = x;
+                last_y = y;
+            }
+        }
+    }
+    grid.insert_col(0, vec!['.'; grid.rows()]);
+    let abyss = grid.rows() - 1;
+    let start_col = y_modifier - 1;
+    return (grid, abyss, start_col);
+}
+
+fn simulate_sand_part1(
+    cave: Grid<char>,
+    abyss: usize,
+    start_col: usize,
+    start: usize,
+) -> Grid<char> {
+    let mut grid = cave.clone();
+    let start = start - start_col;
+    let mut x: usize = 0;
+    let mut y = start;
+    loop {
+        if x >= abyss {
+            break;
+        }
+        if grid.get(x + 1, y).unwrap().to_owned() == '#'
+            || grid.get(x + 1, y).unwrap().to_owned() == 'o'
+        {
+            if grid.get(x + 1, y - 1).unwrap().to_owned() == '#'
+                || grid.get(x + 1, y - 1).unwrap().to_owned() == 'o'
+            {
+                if grid.get(x + 1, y + 1).unwrap().to_owned() == '#'
+                    || grid.get(x + 1, y + 1).unwrap().to_owned() == 'o'
+                {
+                    if x != 0 {
+                        grid[x][y] = 'o';
+                        x = 0;
+                        y = start;
+                    } else {
+                        break;
+                    }
+                } else {
+                    y += 1;
+                }
+            } else {
+                y -= 1;
+            }
+        } else {
+            x += 1;
+        }
+    }
+    return grid;
+}
+
+fn make_cave_full(input: String) -> (Grid<char>, usize) {
+    let mut grid: Grid<char> = Grid::from_vec(vec!['.'], 1);
+    for walls in input.lines() {
+        for wall_coordinates in walls.split(" -> ") {
+            let (y, x) = wall_coordinates.split_once(",").unwrap();
+            let x: usize = x.parse().unwrap();
+            let y: usize = y.parse().unwrap();
+            while x + 1 > grid.rows() {
+                grid.push_row(vec!['.'; grid.cols()]);
+            }
+            while y + 1 > grid.cols() {
+                grid.push_col(vec!['.'; grid.rows()]);
+            }
+        }
+    }
+    grid.push_row(vec!['.'; grid.cols()]);
+    grid.push_col(vec!['.'; grid.rows()]);
+    for walls in input.lines() {
+        let mut last = false;
+        let mut last_x: usize = 0;
+        let mut last_y: usize = 0;
+        for wall_coordinates in walls.split(" -> ") {
+            let (y, x) = wall_coordinates.split_once(",").unwrap();
+            let x: usize = x.parse().unwrap();
+            let y: usize = y.parse().unwrap();
+            grid[x][y] = '#';
+            if last == false {
+                last = true;
+                last_x = x;
+                last_y = y;
+            } else {
+                if x != last_x {
+                    if x > last_x {
+                        for u in last_x..x {
+                            grid[u][y] = '#';
+                        }
+                    } else {
+                        for u in x..last_x {
+                            grid[u][y] = '#';
+                        }
+                    }
+                } else {
+                    if y > last_y {
+                        for u in last_y..y {
+                            grid[x][u] = '#';
+                        }
+                    } else {
+                        for u in y..last_y {
+                            grid[x][u] = '#';
+                        }
+                    }
+                }
+                last_x = x;
+                last_y = y;
+            }
+        }
+    }
+    grid.insert_col(0, vec!['.'; grid.rows()]);
+    grid.push_row(vec!['#'; grid.cols()]);
+    let floor = grid.rows();
+    return (grid, floor);
+}
+
+fn simulate_sand_part2(cave: Grid<char>, floor: usize, start: usize) -> Grid<char> {
+    let mut grid = cave.clone();
+    let mut x: usize = 0;
+    let mut y = start;
+    loop {
+        while y + 1 >= grid.cols() {
+            grid.push_col(vec!['.'; grid.rows()]);
+            let cols = grid.cols() - 1;
+            grid[floor - 1][cols] = '#';
+        }
+        if x > floor {
+            break;
+        }
+        if grid.get(0, y).unwrap().to_owned() == 'o'
+            && grid.get(0, y + 1).unwrap().to_owned() == 'o'
+            && grid.get(0, y - 1).unwrap().to_owned() == 'o'
+        {
+            break;
+        }
+        if grid.get(x + 1, y).unwrap().to_owned() == '#'
+            || grid.get(x + 1, y).unwrap().to_owned() == 'o'
+        {
+            if grid.get(x + 1, y - 1).unwrap().to_owned() == '#'
+                || grid.get(x + 1, y - 1).unwrap().to_owned() == 'o'
+            {
+                if grid.get(x + 1, y + 1).unwrap().to_owned() == '#'
+                    || grid.get(x + 1, y + 1).unwrap().to_owned() == 'o'
+                {
+                    if x != 0 {
+                        grid[x][y] = 'o';
+                        x = 0;
+                        y = start;
+                    } else {
+                        break;
+                    }
+                } else {
+                    y += 1;
+                }
+            } else {
+                y -= 1;
+            }
+        } else {
+            x += 1;
+        }
+    }
+    grid[0][start] = 'o';
+    return grid;
+}
+
 fn part1() {
-    let mut result = "";
-    if let Ok(input) = std::fs::read_to_string("./input_sample") {
-    //if let Ok(input) = std::fs::read_to_string("./input") {
-    // Code here
+    let mut result = 0;
+    //if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    if let Ok(input) = std::fs::read_to_string("./input") {
+        let (cave, abyss, start_col) = make_cave(input);
+        let sand_cave = simulate_sand_part1(cave, abyss, start_col, 500);
+        for row in 0..sand_cave.rows() {
+            for col in 0..sand_cave.cols() {
+                if sand_cave.get(row, col).unwrap().to_owned() == 'o' {
+                    result += 1;
+                }
+            }
+        }
     }
     println!("Part1: {}", result);
 }
 fn part2() {
-    let mut result = "";
-    if let Ok(input) = std::fs::read_to_string("./input_sample") {
-    //if let Ok(input) = std::fs::read_to_string("./input") {
-    // Code here
+    let mut result = 0;
+    //if let Ok(input) = std::fs::read_to_string("./input_sample") {
+    if let Ok(input) = std::fs::read_to_string("./input") {
+        let (cave, floor) = make_cave_full(input);
+        let sand_cave = simulate_sand_part2(cave, floor, 501);
+        for row in 0..sand_cave.rows() {
+            for col in 0..sand_cave.cols() {
+                if sand_cave.get(row, col).unwrap().to_owned() == 'o' {
+                    result += 1;
+                }
+            }
+        }
     }
     println!("Part2: {}", result);
 }
